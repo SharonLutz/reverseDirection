c1aaa5bb8496a525ac6dbbf3feead44c39a01187
Sharon M. Lutz <SharonLutz@users.noreply.github.com>
2020-01-30 08:06:45 -0500

    Update reverseDirection.R

--- R/reverseDirection.R
+++ R/reverseDirection.R
@@ -1,8 +1,9 @@
+
 reverseDirection <-
 function(nSim=1000,n=100,MAF=0.5,gamma0=0,gammaG=0.2,varX=1,
 measurementError=F,delta0=0,deltaX=1,varME=1,
 beta0=0,betaX=seq(from=0,to=1,length.out=4),pleiotropy=F,betaG=1,varY=0.2,
-sig.level=0.05,SEED=1,plot.pdf=T,plot.name="reverseDirection.pdf"){
+TwoSample=F,sig.level=0.05,SEED=1,plot.pdf=T,plot.name="reverseDirection.pdf"){
  
 ################################################################################   
 # load libraries
@@ -18,40 +19,44 @@ sig.level=0.05,SEED=1,plot.pdf=T,plot.name="reverseDirection.pdf"){
 ################################################################################
 # Simple Error checks
 ################################################################################
-if(n<0 | n==0 | floor(n)!=ceiling(n) ){stop("n deltast be an integer greater than 0")}
-if(nSim<0 | nSim==0 | floor(nSim)!=ceiling(nSim) ){stop("nSim deltast be an integer greater than 0")}
-if(SEED<0 | SEED==0 | floor(SEED)!=ceiling(SEED) ){stop("SEED deltast be an integer greater than 0")}
-if(length(MAF)!=1| min(MAF)<0 | max(MAF)>1){stop("MAF deltast be a single value greater than 0 and less than 1")}
-if(length(sig.level)!=1| sig.level<0 | sig.level>1){stop("sig.level deltast be a single value greater than 0 and less than 1")}
-if(length(unique(betaX))<2){stop("betaX deltast be a vector with at least two values")}
-if(!varX>0){stop("varX deltast be greater than 0")}
-if(!varY>0){stop("varY deltast be greater than 0")}
-if(!varME>0){stop("varME deltast be greater than 0")} 
+if(n<0 | n==0 | floor(n)!=ceiling(n) ){stop("n must be an integer greater than 0")}
+if(nSim<0 | nSim==0 | floor(nSim)!=ceiling(nSim) ){stop("nSim must be an integer greater than 0")}
+if(SEED<0 | SEED==0 | floor(SEED)!=ceiling(SEED) ){stop("SEED must be an integer greater than 0")}
+if(length(MAF)!=1| min(MAF)<0 | max(MAF)>1){stop("MAF must be a single value greater than 0 and less than 1")}
+if(length(sig.level)!=1| sig.level<0 | sig.level>1){stop("sig.level must be a single value greater than 0 and less than 1")}
+if(length(unique(betaX))<2){stop("betaX must be a vector with at least two values")}
+if(!varX>0){stop("varX must be greater than 0")}
+if(!varY>0){stop("varY must be greater than 0")}
+if(!varME>0){stop("varME must be greater than 0")} 
 
-if(length(MAF)!=1){stop("length(MAF) deltast equal 1")}
-if(length(gamma0)!=1){stop("length(gamma0) deltast equal 1")}
-if(length(gammaG)!=1){stop("length(gammaG) deltast equal 1")}
-if(length(varX)!=1){stop("length(varX) deltast equal 1")}
+if(length(MAF)!=1){stop("length(MAF) must equal 1")}
+if(length(gamma0)!=1){stop("length(gamma0) must equal 1")}
+if(length(gammaG)!=1){stop("length(gammaG) must equal 1")}
+if(length(varX)!=1){stop("length(varX) must equal 1")}
 
-if(length(delta0)!=1){stop("length(delta0) deltast equal 1")}
-if(length(deltaX)!=1){stop("length(deltaX) deltast equal 1")}
-if(length(varME)!=1){stop("length(varME) deltast equal 1")}
+if(length(delta0)!=1){stop("length(delta0) must equal 1")}
+if(length(deltaX)!=1){stop("length(deltaX) must equal 1")}
+if(length(varME)!=1){stop("length(varME) must equal 1")}
 
-if(length(beta0)!=1){stop("length() deltast equal 1")}
-if(length(betaG)!=1){stop("length() deltast equal 1")}
-if(length(varY)!=1){stop("length(varY) deltast equal 1")}
+if(length(beta0)!=1){stop("length() must equal 1")}
+if(length(betaG)!=1){stop("length() must equal 1")}
+if(length(varY)!=1){stop("length(varY) must equal 1")}
   
 if(plot.pdf==T|plot.pdf=="T"|plot.pdf=="t"|plot.pdf=="True"|plot.pdf=="true"){plot.pdf<-T}  
 if(plot.pdf==F|plot.pdf=="F"|plot.pdf=="f"|plot.pdf=="False"|plot.pdf=="false"){plot.pdf<-F} 
-if(plot.pdf!=T & plot.pdf!=F){stop("plot.pdf deltast equal True or False. Please not that R is case sensitive.")}
+if(plot.pdf!=T & plot.pdf!=F){stop("plot.pdf must equal True or False. Please not that R is case sensitive.")}
+
+if(TwoSample==T|TwoSample=="T"|TwoSample=="t"|TwoSample=="True"|TwoSample=="true"){TwoSample<-T}  
+if(TwoSample==F|TwoSample=="F"|TwoSample=="f"|TwoSample=="False"|TwoSample=="false"){TwoSample<-F} 
+if(TwoSample!=T & TwoSample!=F){stop("TwoSample must equal True or False. Please not that R is case sensitive.")}
 
 if(pleiotropy==T|pleiotropy=="T"|pleiotropy=="t"|pleiotropy=="True"|pleiotropy=="true"){pleiotropy<-T}  
 if(pleiotropy==F|pleiotropy=="F"|pleiotropy=="f"|pleiotropy=="False"|pleiotropy=="false"){pleiotropy<-F} 
-if(pleiotropy!=T & pleiotropy!=F){stop("pleiotropy deltast equal True or False. Please not that R is case sensitive.")}
+if(pleiotropy!=T & pleiotropy!=F){stop("pleiotropy must equal True or False. Please not that R is case sensitive.")}
 
 if(measurementError==T|measurementError=="T"|measurementError=="t"|measurementError=="True"|measurementError=="true"){measurementError<-T}  
 if(measurementError==F|measurementError=="F"|measurementError=="f"|measurementError=="False"|measurementError=="false"){measurementError<-F} 
-if(measurementError!=T & measurementError!=F){stop("measurementError deltast equal True or False. Please not that R is case sensitive.")}
+if(measurementError!=T & measurementError!=F){stop("measurementError must equal True or False. Please not that R is case sensitive.")}
 
 ################################################################################
 # Matrix to save Results
@@ -77,9 +82,9 @@ if(measurementError!=T & measurementError!=F){stop("measurementError deltast equ
 ################################################################################
 # generate data
 ################################################################################                            
-        # Generate G (1 SNP)
-		 G <- rbinom(n,2,MAF)
-
+ 		if(TwoSample==F){
+ 		# Generate G (1 SNP)
+		G <- rbinom(n,2,MAF)
         # Generate X (exposure/ intermediate phenotype)
         Xtrue <- rnorm(n,(gamma0 + gammaG*G),sqrt(varX))
 		if(measurementError==T){X<-rnorm(n,(delta0+deltaX*Xtrue),sqrt(varME))}
@@ -87,11 +92,33 @@ if(measurementError!=T & measurementError!=F){stop("measurementError deltast equ
 		
 		# Generate Y (outcome)
         if(pleiotropy==T){Y <- rnorm(n,(beta0 + betaX[bX]*Xtrue +betaG*G),sqrt(varY))}
-         if(pleiotropy==F){Y <- rnorm(n,(beta0 + betaX[bX]*Xtrue),sqrt(varY))}
-                
+        if(pleiotropy==F){Y <- rnorm(n,(beta0 + betaX[bX]*Xtrue),sqrt(varY))}
+        
         matR[bX,"corGY"]<-matR[bX,"corGY"]+cor(G,Y)
         matR[bX,"corGX"]<-matR[bX,"corGX"]+cor(G,X)
         matR[bX,"corXY"]<-matR[bX,"corXY"]+cor(X,Y)
+        }
+          
+          
+        if(TwoSample==T){
+        	# Generate G1 from sample 1 (1 SNP)
+		G <- rbinom(n,2,MAF)
+        # Generate X (exposure/ intermediate phenotype)
+        Xtrue <- rnorm(n,(gamma0 + gammaG*G),sqrt(varX))
+		if(measurementError==T){X<-rnorm(n,(delta0+deltaX*Xtrue),sqrt(varME))}
+		if(measurementError==F){X<-Xtrue}
+		
+		# Generate G2 from sample 2 (1 SNP)
+		G2 <- rbinom(n,2,MAF)
+		# Generate Y (outcome)
+        if(pleiotropy==T){Y <- rnorm(n,(beta0 + betaX[bX]*Xtrue +betaG*G2),sqrt(varY))}
+        if(pleiotropy==F){Y <- rnorm(n,(beta0 + betaX[bX]*Xtrue),sqrt(varY))}
+        
+        matR[bX,"corGY"]<-matR[bX,"corGY"]+cor(G2,Y)
+        matR[bX,"corGX"]<-matR[bX,"corGX"]+cor(G,X)
+        matR[bX,"corXY"]<-matR[bX,"corXY"]+cor(X,Y)
+        }
+                
 
 ################################################################################
 # MR p-value
@@ -108,10 +135,19 @@ if(pMR<sig.level){matR[bX,"MR"]<-matR[bX,"MR"]+1}
 ################################################################################
 #Steiger correlation
 ################################################################################
+if(TwoSample==F){
 #william test of correlation from Steiger paper
 Stest<-r.test(n, r12 = abs(cor(G,X)), r13 = abs(cor(G,Y)),r23=abs(cor(X,Y)),twotailed = TRUE)
 Z<-Stest$t
 pSteiger<-Stest$p
+}
+
+ if(TwoSample==T){
+#william test of correlation from Steiger paper
+Stest<-r.test(n, r12 = abs(cor(G,X)), r34 = abs(cor(G2,Y)),twotailed = TRUE)
+Z<-Stest$z
+pSteiger<-Stest$p
+}
 
 if(Z>0){matR[bX,"Z+"]<-matR[bX,"Z+"]+1}
 if(pSteiger<sig.level){matR[bX,"Steiger"]<-matR[bX,"Steiger"]+1}
@@ -124,7 +160,7 @@ if(pSteiger<sig.level & pMR<sig.level){
 if(Z>0){matR[bX,"case1"]<-matR[bX,"case1"]+1}
 if(Z<0){matR[bX,"case2"]<-matR[bX,"case2"]+1}
 }else{matR[bX,"case3"]<-matR[bX,"case3"]+1}
-         
+          
 ################################################################################
 # end loops
 ################################################################################    
